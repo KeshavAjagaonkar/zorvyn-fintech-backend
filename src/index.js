1c0d629ae93c5406106f5fa2ed4dde7eb18a2c8f
KeshavAjagaonkar <keshavvamanajagaonkar@gmail.com>
2026-04-06 13:04:41 +0530

    added cors

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
+import cors from 'cors';
+import authroutes from './routes/auth.routes.js';
+
 
 const app = express();
 
+app.use(cors());
+
 app.use(express.json());
+app.use('/auth', authroutes);
 
 
 
