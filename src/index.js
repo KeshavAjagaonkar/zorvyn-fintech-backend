c09052d27f6535cb96347fc479567a8f85fba0f9
KeshavAjagaonkar <keshavvamanajagaonkar@gmail.com>
2026-04-06 14:06:02 +0530

    added the record routes to index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import express from 'express';
 import cors from 'cors';
 import authroutes from './routes/auth.routes.js';
 import userRoutes from './routes/user.routes.js';
+import recordRoutes from './routes/record.routes.js';
 
 
 const app = express();
@@ -12,7 +13,8 @@ app.use(cors());
 
 app.use(express.json());
 app.use('/auth', authroutes);
-app.use('/users', userRoutes)
+app.use('/users', userRoutes);
+app.use('/records', recordRoutes);
 
 
 const PORT  = process.env.PORT || 3000;
